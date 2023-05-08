5fa68081e06b3b734b29ee94a99ae3f176f0d089
UltraRossa <fa2um@yandex.ru>
2023-05-08 04:24:58 +0300

    fix brain-games.js package-lock.json package.json cli.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import greetings from "../src/cli.js";
+import greetings from '../src/cli.js';
 
 console.log('node bin/brain-games.js\nWelcome to the Brain Games!');
 greetings();
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const greetings = () => {
-    let userName = readlineSync.question('May I have you name? ');
-    console.log(`Hello, ${userName}!`);
-}
+  const userName = readlineSync.question('May I have you name? ');
+  console.log(`Hello, ${userName}!`);
+};
 
 export default greetings;
