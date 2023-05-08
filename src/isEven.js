c5328beae3e9ec1086c1d2413c9ce15d3c404bd8
UltraRossa <fa2um@yandex.ru>
2023-05-08 23:21:43 +0300

    add brain-even.js isEven.js

---
+++ src/isEven.js
@@ -0,0 +1,21 @@
+import readlineSync from 'readline-sync';
+import greetings from './cli.js';
+
+const isEven = () => {
+  const userName = greetings();
+  const numbers = [15, 6, 7];
+
+  console.log("Answer 'yes' if given number is even. Otherwise answer 'no'.");
+
+  for (let i = 0; i < numbers.length; i += 1) {
+    console.log(`Question: ${numbers[i]}`);
+    const correctAnswer = numbers[i] % 2 === 0 ? 'yes' : 'no';
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default isEven;
