562aff0476cde28e7168d342702f1f04dddef749
UltraRossa <fa2um@yandex.ru>
2023-05-08 23:21:20 +0300

    fix Makefile brain-games.js package.json cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const greetings = () => {
-  const userName = readlineSync.question('May I have you name? ');
+  const userName = readlineSync.question('Welcome to the Brain Games!\nMay I have you name? ');
   console.log(`Hello, ${userName}!`);
+  return userName;
 };
 
 export default greetings;
