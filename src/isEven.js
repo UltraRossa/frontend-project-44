75ba52edee678fa686048c9b428888174415f608
UltraRossa <fa2um@yandex.ru>
2023-05-09 03:15:11 +0300

    fix serc/isEven.js

--- src/isEven.js
+++ src/isEven.js
@@ -3,13 +3,14 @@ import greetings from './cli.js';
 
 const isEven = () => {
   const userName = greetings();
-  const numbers = [15, 6, 7];
+  const numberOfTries = 3;
 
   console.log("Answer 'yes' if given number is even. Otherwise answer 'no'.");
 
-  for (let i = 0; i < numbers.length; i += 1) {
-    console.log(`Question: ${numbers[i]}`);
-    const correctAnswer = numbers[i] % 2 === 0 ? 'yes' : 'no';
+  for (let i = 0; i < numberOfTries; i += 1) {
+    const question = Math.floor(Math.random() * 101);
+    console.log(`Question: ${question}`);
+    const correctAnswer = question % 2 === 0 ? 'yes' : 'no';
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer === correctAnswer) {
       console.log('Correct!');
