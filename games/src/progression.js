c2c2abe6f5fee688e813fdd5d04c9e1f83c3eff6
UltraRossa <fa2um@yandex.ru>
2023-05-10 04:08:51 +0300

    add progression.js

---
+++ games/src/progression.js
@@ -0,0 +1,26 @@
+import checkAnswer from './checkAnswer.js';
+
+const progression = (userName) => {
+  const minLength = 5;
+  const maxLength = 10;
+  const progressionLength = Math.floor(Math.random() * (maxLength - minLength)) + minLength;
+  const plus = Math.floor(Math.random() * 11);
+  const progressionNumbers = [];
+  const replacedIndex = Math.floor(Math.random() * progressionLength);
+
+  for (let i = 0; i < progressionLength; i += 1) {
+    if (i === 0) {
+      progressionNumbers[i] = (Math.floor(Math.random() * 11)) + plus;
+    } else {
+      progressionNumbers[i] = progressionNumbers[i - 1] + plus;
+    }
+  }
+
+  const correctAnswer = progressionNumbers[replacedIndex];
+  progressionNumbers[replacedIndex] = '..';
+  const question = progressionNumbers.join(' ');
+
+  return checkAnswer(question, correctAnswer, userName);
+};
+
+export default progression;
