2307f3747597d6c62bc5c75923b6c5de1de79640
UltraRossa <fa2um@yandex.ru>
2023-05-10 04:20:09 +0300

    fix progression.js

--- games/src/progression.js
+++ games/src/progression.js
@@ -4,13 +4,13 @@ const progression = (userName) => {
   const minLength = 5;
   const maxLength = 10;
   const progressionLength = Math.floor(Math.random() * (maxLength - minLength)) + minLength;
-  const plus = Math.floor(Math.random() * 11);
+  const plus = Math.floor((Math.random() * 10) + 1);
   const progressionNumbers = [];
   const replacedIndex = Math.floor(Math.random() * progressionLength);
 
   for (let i = 0; i < progressionLength; i += 1) {
     if (i === 0) {
-      progressionNumbers[i] = (Math.floor(Math.random() * 11)) + plus;
+      progressionNumbers[i] = (Math.floor(Math.random() * 10));
     } else {
       progressionNumbers[i] = progressionNumbers[i - 1] + plus;
     }
